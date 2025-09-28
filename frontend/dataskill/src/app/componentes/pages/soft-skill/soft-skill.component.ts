8da9db06726408aa16c6a90a02d06f2c57bbadd4
Johnatan Coelho <johnatansouza242@gmail.com>
2025-09-28 01:29:51 -0300

    ✨ feat: #11 Telas hard-skill, soft-skill e alteracao da página perfil

---
+++ frontend/dataskill/src/app/componentes/pages/soft-skill/soft-skill.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-soft-skill',
+  standalone: true,
+  imports: [],
+  templateUrl: './soft-skill.component.html',
+  styleUrl: './soft-skill.component.css'
+})
+export class SoftSkillComponent {
+
+}
