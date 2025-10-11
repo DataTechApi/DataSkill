788210312e0645932f9147b7e23149aaa7cb01a0
Johnatan Coelho <johnatansouza242@gmail.com>
2025-10-10 22:23:25 -0300

    :wastebasket: remove: #41 Tela de cadastro na parte externa da aplicação removida

--- frontend/dataskill/src/app/app.routes.ts
+++ frontend/dataskill/src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { IndexComponent } from './componentes/pages/index/index.component';
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './componentes/pages/login/login.component';
-import { CadastroComponent } from './componentes/pages/cadastro/cadastro.component';
 import { PerfilComponent } from './componentes/pages/perfil/perfil.component';
 import { HardSkillComponent } from './componentes/pages/hard-skill/hard-skill.component';
 import { SoftSkillComponent } from './componentes/pages/soft-skill/soft-skill.component';
@@ -12,7 +11,6 @@ import { CertificadoComponent } from './componentes/pages/certificado/certificad
 export const routes: Routes = [
   { path: '', component:IndexComponent},
   { path: 'login', component:LoginComponent},
-  { path: 'cadastro', component:CadastroComponent},
   { path: 'perfil', component:PerfilComponent},
   {path: 'hard-skill', component:HardSkillComponent},
   {path: 'soft-skill', component:SoftSkillComponent},
--- frontend/dataskill/src/app/componentes/pages/login/login.component.ts
+++ frontend/dataskill/src/app/componentes/pages/login/login.component.ts
@@ -8,32 +8,8 @@ import { LoginDTORequest } from '../../../models/LoginDTORequest';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
   logodataskill:string = "assets/dataskill.png"
 
-  LoginData: LoginDTORequest = {
-    nome: '',
-    senha: ''
-  };
 
-  constructor(private loginService: LoginService, private router: Router) { }
-
-  ngOnInit(): void {
-  }
-
-  onSubmit(): void {
-    this.loginService.login(this.LoginData.nome, this.LoginData.senha).subscribe(
-      (response: any) => {
-        // Sucesso no login
-        console.log('Login bem-sucedido:', response);
-        // Redirecionar para a página desejada após o login
-        this.router.navigate(['/perfil']);
-      },
-      (error: any) => {
-        // Erro no login
-        console.error('Erro no login:', error);
-        alert('Nome ou senha incorretos. Tente novamente.');
-      }
-    );
-  }
 }
