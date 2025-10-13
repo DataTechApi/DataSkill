f53224f5a2502a5d59671049eded51808a297b2b
Johnatan Coelho <johnatansouza242@gmail.com>
2025-10-13 20:50:18 -0300

    :bug: fix: #37 Erro Cross resolvido

--- backend/dataskill/src/main/java/com/datatech/dataskill/controller/UsuarioController.java
+++ backend/dataskill/src/main/java/com/datatech/dataskill/controller/UsuarioController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/usuario")
 @Tag(name = "usuario")
