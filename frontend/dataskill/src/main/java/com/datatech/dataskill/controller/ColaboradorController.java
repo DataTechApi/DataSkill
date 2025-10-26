193e90e7777a6d2f3323d153da1d90abce99f5c7
Jackson Moraes <jacksontadeu@hotmail.com>
2025-10-26 14:23:36 -0300

    evert "::spakles: feat: #41 Criacao dos templates de tela de cadastro de softskill,hardskill,experiencia,autoavaliacao e certificado"
    This reverts commit a32c2f6abec62005c4732005738fcda918cc9270.
    
    ":sparkles: feat:#41 Criacao dos templates de tela de cadastro de softskill,hardskill,experiencia,autoavaliacao e certificado"

--- frontend/dataskill/src/main/java/com/datatech/dataskill/controller/ColaboradorController.java
+++ frontend/dataskill/src/main/java/com/datatech/dataskill/controller/ColaboradorController.java
@@ -10,25 +10,5 @@ public class ColaboradorController {
     public String perfil() {
         return "colaborador/perfil";
     }
-    @GetMapping("/colaborador/cadastrar-hardskill")
-    public String cadastrarHardSkill() {
-        return "colaborador/cadastrar-hardskill";
-    }       
-    @GetMapping("/colaborador/cadastrar-certificado")
-    public String cadastrarCertificado() {
-        return "colaborador/cadastrar-certificado";
-    }       
-    @GetMapping("/colaborador/cadastrar-experiencia")
-    public String cadastrarExperiencia() {
-        return "colaborador/cadastrar-experiencia";
-    }       
-    @GetMapping("/colaborador/cadastrar-softskill")
-    public String cadastrarSoftskill() {
-        return "colaborador/cadastrar-softskill";
-    }       
-    @GetMapping("/colaborador/cadastrar-autoavaliacao")
-    public String cadastrarAutoavaliacao() {
-        return "colaborador/cadastrar-autoavaliacao";
-    }       
 
 }
